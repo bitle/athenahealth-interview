7b500e90461f2e2cba62e94b1fa1071b134f6fbe
Damir Suleymanov <damir@cloudlock.com>
2017-02-26 11:29:16 -0500

    Add Card.order_value method
    This method will be used to sort the results

--- athenahealth/deckofcards.py
+++ athenahealth/deckofcards.py
@@ -62,3 +62,22 @@ class Card(object):
     def __init__(self, value):
         super(Card, self).__init__()
         self.value = value
+
+    @property
+    def order_value(self):
+        order = {
+            'ACE': 1,
+            '2': 2,
+            '3': 3,
+            '4': 4,
+            '5': 5,
+            '6': 6,
+            '7': 7,
+            '8': 8,
+            '9': 9,
+            '10': 10,
+            'JACK': 11,
+            'QUEEN': 12,
+            'KING': 13
+        }
+        return order[self.value]

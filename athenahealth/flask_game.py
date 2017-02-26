b4d173cb5882d6d5dd319d45f36f915272873c63
Damir Suleymanov <damir@cloudlock.com>
2017-02-26 14:14:50 -0500

    Add error handling

--- athenahealth/flask_game.py
+++ athenahealth/flask_game.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-from flask import g
 
-from athenahealth.deckofcards import NoMoreCardsError
+from athenahealth.deckofcards import NoMoreCardsError, DeckOfCardsError
 from athenahealth.game import Game
 
 app = Flask(__name__)
@@ -18,21 +17,26 @@ def index():
 @app.route('/', methods=["POST"])
 def form_submit():
     next_move = request.form.get('next-move')
-    if next_move == 'reshuffle':
-        game.reshuffle()
-    elif next_move == 'draw':
-        game.next_card()
-    else:
-        app.logger.info("Unexpected next move")
+    try:
+        if next_move == 'reshuffle':
+            game.reshuffle()
+        elif next_move == 'draw':
+            game.next_card()
+        else:
+            app.logger.info("Unexpected next move")
+    except NoMoreCardsError:
+        return render_index(error="No more cards left in the deck")
+    except DeckOfCardsError:
+        return render_index(error="Unexpected error. Please try again.")
 
     return render_index()
 
 
-def render_index():
+def render_index(error=None):
     results = []
     for card, count in game.get_results().iteritems():
         results.append((card.value, count))
-    return render_template("index.html", results=results)
+    return render_template("index.html", results=results, error=error)
 
 
 if __name__ == "__main__":

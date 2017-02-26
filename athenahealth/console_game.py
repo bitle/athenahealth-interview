dcbd10cc9f1b89e7f527a1950bf9aff297e941a5
Damir Suleymanov <damir@cloudlock.com>
2017-02-26 11:48:53 -0500

    Print pretty results

--- athenahealth/console_game.py
+++ athenahealth/console_game.py
@@ -12,7 +12,7 @@ def main():
             card = game.next_card()
             print "Your card is: %s" % card.value
         elif input == "reshuffle":
-            print "Your result is: %s" % game.get_results()
+            print "Your result is: %s" % pretty_results(game.get_results())
             game.reshuffle()
             print "New game started"
         elif input in ["quit", "exit", "q", "bye"]:
@@ -20,6 +20,14 @@ def main():
             break
         else:
             print "Unrecognized input"
+
+
+def pretty_results(results):
+    result_string = "\n"
+    for card, count in results.iteritems():
+        result_string += "%s: %d\n" % (card, count)
+
+    return result_string
 
 
 if __name__ == '__main__':
